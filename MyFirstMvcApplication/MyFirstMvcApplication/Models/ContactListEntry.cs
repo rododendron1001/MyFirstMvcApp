af54bd16afd75318e215a803d8cd2950bd90dc7a
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-08-14 00:42:20 +0300

    working on

--- MyFirstMvcApplication/MyFirstMvcApplication/Models/ContactListEntry.cs
+++ MyFirstMvcApplication/MyFirstMvcApplication/Models/ContactListEntry.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyFirstMvcApplication.Models
 {
     public class ContactListEntry
     {
+        [Key]
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public ContactType ContactType { get; set; }
+
+        [Required]
         public string Name { get; set; }
 
+        [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
+
+        [StringLength(50)]
         public string PhoneNumber { get; set; }
+
+        [StringLength(50)]
         public string Email { get; set; }
            
     }
