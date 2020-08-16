cf5dd04dd4d64b0e1cdcc0d835dd546f4da42a03
rododendron1001 <60620826+rododendron1001@users.noreply.github.com>
2020-08-16 15:23:43 +0300

    My First MVC App

---
+++ MyFirstMvcApplication/MyFirstMvcApplication/Migrations/20200816121048_UpdateContactList_AddConstraints.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyFirstMvcApplication.Migrations
+{
+    public partial class UpdateContactList_AddConstraints : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "ContactListEntry",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactListEntry",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ContactListEntry",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "ContactListEntry",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "ContactListEntry",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "ContactListEntry",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
--- MyFirstMvcApplication/MyFirstMvcApplication/Models/ContactListEntry.cs
+++ MyFirstMvcApplication/MyFirstMvcApplication/Models/ContactListEntry.cs
@@ -13,18 +13,26 @@ namespace MyFirstMvcApplication.Models
         public int Id { get; set; }
 
         [Required]
+        [Display(Name = "Contact Type")]
         public ContactType ContactType { get; set; }
 
         [Required]
+        [StringLength(100)]
+        [Display(Name = "Name")]
         public string Name { get; set; }
 
+        [Display(Name = "Date of Birth")]
         [DataType(DataType.Date)]
         public DateTime? DateOfBirth { get; set; }
 
+        [Display(Name = "Phone Number")]
         [StringLength(50)]
+        [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
+        [Display(Name="Email")]
         [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
            
     }
